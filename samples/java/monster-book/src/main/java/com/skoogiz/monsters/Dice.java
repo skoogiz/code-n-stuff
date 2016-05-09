e3cc9afee8f5c2e4298f0f4633871921da6df6d6
Daniel <danielsaka@hotmail.com>
2016-05-09 22:41:00 +0200

    Sittning med herr Saka

---
+++ samples/java/monster-book/src/main/java/com/skoogiz/monsters/Dice.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.skoogiz.monsters;
+
+import java.util.Random;
+
+/**
+ * @author PC
+ *
+ */
+public class Dice {
+
+	private final int sides;
+	
+	private Random random;
+
+	private int currentValue;
+	
+	public Dice(int sides) {
+		this.sides = sides;
+		this.random = new Random();
+	}
+
+	public int roll() {
+		return (currentValue = random.nextInt(sides) + 1);
+	}
+
+	public int getSides() {
+		return sides;
+	}
+
+	public int getCurrentValue() {
+		return currentValue;
+	}
+
+}
