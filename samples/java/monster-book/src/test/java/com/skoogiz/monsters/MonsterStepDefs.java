52f4fe429fcf66423694a9b8ea53674c39bcf394
skoogiz <skoogiz@gmail.com>
2016-06-03 00:00:10 +0200

    Monster Template refactoring

--- samples/java/monster-book/src/test/java/com/skoogiz/monsters/MonsterStepDefs.java
+++ samples/java/monster-book/src/test/java/com/skoogiz/monsters/MonsterStepDefs.java
@@ -3,9 +3,16 @@
  */
 package com.skoogiz.monsters;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+import com.skoogiz.monsters.model.MonsterTemplate;
+import com.skoogiz.monsters.model.MonsterTemplate.Builder;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -19,25 +26,67 @@ public class MonsterStepDefs
 
     private MonsterTemplate template;
 
-    private Monster monster;
+    private MonsterTemplate.Builder builder;
 
-    @Given("^I have a monster template$")
-    public void i_have_a_monster_template()
+    @Given("^a monster template builder$")
+    public void a_monster_template_builder() throws Throwable
     {
-        template = new MonsterTemplate.Builder().build();
-
-        assertNotNull(template);
+        builder = new Builder();
     }
 
-    @When("^I generate a monster$")
-    public void i_generate_a_monster()
+    @When("^set race \"([^\"]*)\"$")
+    public void set_race(String race) throws Throwable
     {
-        monster = template.generate();
+        builder.race(race);
     }
 
-    @Then("^a monster is created$")
-    public void a_monster_is_created()
+    @When("^set abilities:$")
+    public void set_abilities(Map<String, String> abilities) throws Throwable
     {
-        assertThat("That we have spawned a Monster", monster, notNullValue());
+        abilities.forEach((name, formula) -> builder.ability(name, formula));
     }
+
+    @When("^set habitats:$")
+    public void set_habitats(List<String> habitats) throws Throwable
+    {
+        habitats.forEach((habitat) -> builder.habitat(habitat));
+    }
+
+    @When("^set rarity \"([^\"]*)\"$")
+    public void set_rarity(String rarity) throws Throwable
+    {
+        builder.rarity(rarity);
+    }
+
+    @When("^set movement on land \"([^\"]*)\"$")
+    public void set_movement_on_land(String movement) throws Throwable
+    {
+        builder.movement(movement);
+    }
+
+    @When("^set natural protection \"(\\d+)\"$")
+    public void set_natural_protection(int protection) throws Throwable
+    {
+        builder.protection(protection);
+    }
+
+    @When("^set numbers they show up in \"([^\"]*)\"$")
+    public void set_numbers_they_show_up_in(String numbers) throws Throwable
+    {
+        builder.numbers(numbers);
+    }
+
+    @When("^build monster template$")
+    public void build_monster_template() throws Throwable
+    {
+        template = builder.build();
+
+    }
+
+    @Then("^a monster template is created$")
+    public void a_monster_template_is_created() throws Throwable
+    {
+        assertThat("template exists", template, is(notNullValue()));
+    }
+
 }
